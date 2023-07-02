3a5d23edcccc002b225df314c2baa70f69752985
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-07-01 22:21:15 -0300

    Primeiro commit: projeto criado

---
+++ src/main/java/com/cursojava/mongodbspringboot/WorkshopMongoApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.mongodbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursojava/mongodbspringboot/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.mongodbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
