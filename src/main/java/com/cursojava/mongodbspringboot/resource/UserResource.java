f9fe9f872e281dc922dc20565005daa36558deca
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-07-03 16:44:05 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/cursojava/mongodbspringboot/resource/UserResource.java
+++ src/main/java/com/cursojava/mongodbspringboot/resource/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,13 @@ public class UserResource {
 		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
+		
+	}
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void>delete(@PathVariable String id){
+		users.delete(id);
+		
+		return ResponseEntity.noContent().build();
 		
 	}
 }
--- src/main/java/com/cursojava/mongodbspringboot/services/UserService.java
+++ src/main/java/com/cursojava/mongodbspringboot/services/UserService.java
@@ -44,4 +44,8 @@ public class UserService {
 		return user;
 		
 	}
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
 }
