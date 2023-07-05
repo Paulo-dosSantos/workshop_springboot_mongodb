6d5c8212b711943c1578ad7efb0085f87ec487a9
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-07-05 17:29:40 -0300

    Referenciando os posts do usuário

--- src/main/java/com/cursojava/mongodbspringboot/config/Instantiation.java
+++ src/main/java/com/cursojava/mongodbspringboot/config/Instantiation.java
@@ -42,6 +42,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 		
 	}
 
